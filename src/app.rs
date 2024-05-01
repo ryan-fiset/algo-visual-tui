2ad99b931fe80f4c4f86ef1740df19a7b87c847b
Ryan Fiset <ryfiset@gmail.com>
2024-04-30 17:40:32 -0700

    Remove unneeded tab logic

--- src/app.rs
+++ src/app.rs
@@ -8,8 +8,6 @@ pub type AppResult<T> = std::result::Result<T, Box<dyn error::Error>>;
 pub struct App {
     /// Is the application running?
     pub running: bool,
-    /// Currently selected tab
-    pub current_tab: usize,
     /// List of tabs
     pub tab_list: Vec<&'static str>,
     /// Data Length
@@ -22,7 +20,6 @@ impl Default for App {
     fn default() -> Self {
         Self {
             running: true,
-            current_tab: 0,
             tab_list: vec!["Bubble Sort", "Bogo Sort", "Selection Sort"],
             data_length: 50,
             tick_rate: 10,
@@ -42,21 +39,5 @@ impl App {
     /// Set running to false to quit the application.
     pub fn quit(&mut self) {
         self.running = false;
-    }
-
-    pub fn next_tab(&mut self) {
-        if self.current_tab != self.tab_list.len() - 1 {
-            self.current_tab += 1;
-        } else {
-            self.current_tab = 0;
-        }
-    }
-
-    pub fn prev_tab(&mut self) {
-        if self.current_tab != 0 {
-            self.current_tab -= 1;
-        } else {
-            self.current_tab = self.tab_list.len() - 1;
-        }
     }
 }
